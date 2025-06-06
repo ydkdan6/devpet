4bd0181ae28747c46efedcb6d55531fb8518658b
ydkdan6 <ydkdan6@gmail.com>
2025-06-06 23:01:18 +0100

    fixing text and images

--- assets/js/plugins/plugins.js
+++ assets/js/plugins/plugins.js
@@ -5415,7 +5415,7 @@
 
 /*!
  * Bootstrap v5.3.3 (https://getbootstrap.com/)
- * Copyright 2011-2024 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
+ * Copyright 2011-2025 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
  */
 !(function (t, e) {
